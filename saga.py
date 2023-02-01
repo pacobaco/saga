2f01ee7779ecf8631adf01abd1c6d34aeeccd074
jwookie2 <jwookie2@hotmail.com>
2023-01-31 23:44:54 -0500

    Add files via upload

--- saga.py
+++ saga.py
@@ -1,93 +1,25 @@
-from yahoofinancials import YahooFinancials
+import numpy as np
+import math
+o = open('c:\\saga\\fullcorpstats.csv')
+o = o.read()
+o = o.split('\n')
 
-def divy(x,y):
-	return x/y
+s = {}
 
-def rund(e):
-	w={}
-	for x in e:
-		if x:
-			r=divy(float(x[1]),float(x[2]))
-			if r in w:
-				w[r].append(x[0])
-				w[r].append(x[3])
-			else:
-				w[r]=[]
-				w[r].append(x[0])
-				w[r].append(x[3])
-	return w
+def cosine_similarity(a, b):
+    return sum([float(i)*float(j) for i,j in zip(a, b)])/(math.sqrt(sum([float(i)*float(i) for i in a]))* math.sqrt(sum([float(i)*float(i) for i in b]))) 
 
-o = open('c:\\saga\\saga')
-r = o.read()
-r=r.replace('"','')
-print(r)
-e=r.split('\n')
+for x in o:
+    y = x.split(',')
+    try:
+        s[y[0]]=[y[1],y[2],y[3],y[5],y[6]]
+    except: continue
 
-#i=open('n2dog')
-#kl=i.read()
-#pok=kl.split('\n')
-#il=len(pok)
-#print kl
-#uo=kl
-#i.close()
+q = {}
 
-sto = []
-
-for x in e[0:6493]:
-	print(x)
-#       i=open('scf','w')
-#       uo=i.read()
-#       try:
-        #x='AAPL'
-#       try:
-	print(x)
-	yf=YahooFinancials(x)
-#       except:
-#               print 'err'
-#               continue
-	m=yf.get_financial_stmts('quarter','balance')
-	print(m)
-	z=list(m.keys())
-	print(z)
-	w=z[0]
-	r=m[w]
-	u=list(r.keys())
-	print(u)
-	f=r[u[0]] 
-	try:
-		h=f[0]
-	except:
-		continue
-	j=list(h.keys())
-	dt=j
-	k=h[j[0]]
-	try:
-		l=k['totalAssets']
-		c=yf.get_ebit()
-	except:
-		continue
-#	uo=uo+x+','+str(c)+','+str(l)+'\n'
-        #bb=bb+uo
-#       i.write(bb)
-#       i.close()
-	s = []
-	s.append(x)
-	s.append(c)
-	s.append(l)
-	s.append(dt)
-	sto.append(s)
-	print(x,c,l,'\n')
-#       except:
-#       continue
-print(sto)  
-print(rund(sto))
-h=rund(sto)
-b=list(h.keys())
-b.sort()
-for x in b:
-	print(x,h[x][0],h[x][1])
-print(len(b))
-#o=open('nasdog','w')
-#o.write(uo)
-#o.close()
-
+for x in list(s.keys()):
+        q[x]={}
+        for y in list(s.keys()):
+            try:
+                q[x][y]=cosine_similarity(s[x],s[y])
+            except: continue
